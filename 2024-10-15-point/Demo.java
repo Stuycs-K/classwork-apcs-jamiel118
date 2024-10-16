ed9ca183bc9bb596760822ac11f24701516d3a67
Jamie Li <jli60@stuy.edu>
2024-10-15 22:46:05 -0400

    reorganized testers and added commets

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -9,16 +9,21 @@ public class Demo{
     }
   
     public static void main(String[]args){
+      //Point constructor
       Point p1 = new Point(1,1);
       Point p2 = new Point(-1,-1);
       Point p3 = new Point(3,4);
-      System.out.println( p3);
       Point p4 = new Point(p3);
       Point origin = new Point(0, 0);
+      //toString
+      System.out.println( p3);
       System.out.println(p4.toString());
+      //distance method in Demo
       System.out.println( distance(p1,p2));
-      System.out.println( Point.distance(p1,p2));
       System.out.println(distance(p3, origin));
+      //distance method in Point
+      System.out.println( Point.distance(p1,p2));
+      //distanceTo non-static method
       System.out.println(p3.distanceTo(origin));
       System.out.println( p1.distanceTo(p2));
   
