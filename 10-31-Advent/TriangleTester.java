a0987f09de57a2771c31686f7dd810d4dd7ee8c1
Jamie Li <jli60@stuy.edu>
2024-10-31 11:01:55 -0400

    finished counterTrianglesA method

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -2,12 +2,22 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
 public class TriangleTester.java {
 
   Scanner sc = new Scanner("inputTri.txt");
 
-  public static int countTrianglesA(String filename) {
+  public static boolean helper(String filename) {
+    while()
 
+  }
+
+  public static int countTrianglesA(String filename) {
+    int counter = 0;
+    while(input.hasNextLine()) {
+      if (helper(inputTri.txt.nexLine())) {
+        counter += 0;
+      }
+    }
+    return counter;
   }
 }
