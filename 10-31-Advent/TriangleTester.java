a73334b5bd7bab3aa56f717a1692e9719dc88954
Jamie Li <jli60@stuy.edu>
2024-10-31 10:53:39 -0400

    created txt file and started counTrianglesA method

---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
+public class TriangleTester.java {
+
+  Scanner sc = new Scanner("inputTri.txt");
+
+  public static int countTrianglesA(String filename) {
+
+  }
+}
