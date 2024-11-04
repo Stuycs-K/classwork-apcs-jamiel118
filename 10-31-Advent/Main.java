e440033f0913e555288dceefed205ffaafb38428
Jamie Li <jli60@stuy.edu>
2024-11-03 22:52:34 -0500

    added tester case for FileNotFoundException

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -2,5 +2,6 @@
 class Main {
     public static void main(String[] args) {
       System.out.println(TriangleTester.countTrianglesA("inputTri.txt"));
+      System.out.println(TriangleTester.countTrianglesA("hi.txt"));
   }
 }
