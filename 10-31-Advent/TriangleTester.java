032db1c332af9f540ce944a300d542c8db06e74e
Jamie Li <jli60@stuy.edu>
2024-11-03 22:45:36 -0500

    fixed countTrianglesA method to read files

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,27 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class TriangleTester {
 
-  Scanner sc = new Scanner("inputTri.txt");
-
   public static boolean helper(String input) {
+    Scanner sc = new Scanner(input);
     int[] ary = new int[3];
-    int count = 3;
-    for (int i = 0; i < count; i++) {
-      ary[i] = input.nextInt();
+    int count = 0;
+    while(sc.hasNextInt()) {
+      ary[count] = sc.nextInt();
+      count++;
     }
-    return (ary[0] + ary[1] <= ary[2] || ary[0] + ary[2] <= ary[1] || ary[1] + ary[2] <= ary[0] );
+    return (ary[0] + ary[1] > ary[2] && 
+            ary[0] + ary[2] > ary[1] && 
+            ary[1] + ary[2] > ary[0]);
   }
+
 
   public static int countTrianglesA(String filename) {
     int counter = 0;
-    while(filename.hasNextLine()) {
-      if (helper("inputTri.txt".nexLine())) {
-        counter += 0;
+    try {
+      File file = new File(filename);
+      Scanner sc = new Scanner(file);
+      while(sc.hasNextLine()) {
+        if (helper(sc.nextLine())) {
+          counter += 1;
+        }
       }
+      sc.close();
+
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
     }
-    return counter;
+    return counter; 
   }
+  
 }
