bd43dbb266eee60076da5ed029f1fdbe8fd3989c
Jamie Li <jli60@stuy.edu>
2024-11-18 00:18:09 -0500

    created makeReversedList method and tested

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -23,4 +23,14 @@ public class ArrayListPractice{
 
   }
 
+  public static ArrayList<String> makeReversedList( ArrayList<String> original){
+    //return a new ArrayList that is in the reversed order of the original.
+    ArrayList<String> result = new ArrayList<String>(original.size());
+    for (int i = original.size() - 1; i > -1; i--) {
+        result.add(original.get(i));
+    }
+    return result;
+  } 
+  
+
 }
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -2,14 +2,17 @@ import java.util.ArrayList;
 public class Driver{
   public static void main(String[] args){
     ArrayList<String> a = ArrayListPractice.createRandomArray(200000);
-    for (int i = 0; i < 200000; i++) {
-      System.out.println(a.get(i));
-    }
+    System.out.println(a);
+    ArrayList<String> b = ArrayListPractice.createRandomArray(10);
+    System.out.println(b);
 
     ArrayListPractice.replaceEmpty(a);
-    for (int i = 0; i < 200000; i++) {
-      System.out.println(a.get(i));
-    }
+    System.out.println(a);
+    ArrayListPractice.replaceEmpty(b);
+    System.out.println(b);
+
+    ArrayList<String> c = ArrayListPractice.makeReversedList(b);
+    System.out.println(c);
 
   }
 }
