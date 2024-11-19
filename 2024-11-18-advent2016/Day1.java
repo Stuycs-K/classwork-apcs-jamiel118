45cacca21aa96d5a11d67f84abe8db090380242e
Jamie Li <jli60@stuy.edu>
2024-11-18 20:04:01 -0500

    completed Part 1 of Day 1

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,28 +1,72 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 
 public class Day1 {
-  private int x;
-  private int y
 
-  public static int readFile(String filename) {
+  public static  ArrayList<String> readFile(String filename) {
     ArrayList<String> result = new ArrayList<String>();
     try {
       File file = new File(filename);
       Scanner sc = new Scanner(file);
       while(sc.hasNextLine()) {
-        String line = in.nextLine();
-        String[]nums = line.split(", ")
+        String line = sc.nextLine();
+        ArrayList<String>nums = new ArrayList<String>(Arrays.asList(line.split(", ")));
+        result.addAll(nums);
         }
-      }
-      sc.close();
-
-    } catch (FileNotFoundException ex) {
+        sc.close();
+      } catch (FileNotFoundException ex) {
       System.out.println("File not found");
     }
-    return counter;
+    return result;
+  }
+
+  public static int EasterBunny(String filename) {
+    int x = 0;
+    int y = 0;
+    String direction = "north";
+    ArrayList<String> lst = readFile(filename);
+    for (int i = 0; i < lst.size(); i++) {
+      if (direction.equals("north")) {
+        if (lst.get(i).contains("R")) {
+          direction = "east";
+          x += Integer.valueOf(lst.get(i).substring(1));
+        } else {
+          direction = "west";
+          x -= Integer.valueOf(lst.get(i).substring(1));
+        }
+      }
+      else if (direction.equals("south")) {
+        if (lst.get(i).contains("L")) {
+          direction = "east";
+          x += Integer.valueOf(lst.get(i).substring(1));
+        } else {
+          direction = "west";
+          x -= Integer.valueOf(lst.get(i).substring(1));
+        }
+      }
+      else if (direction.equals("east")) {
+        if (lst.get(i).contains("R")) {
+          direction = "south";
+          y -= Integer.valueOf(lst.get(i).substring(1));
+        } else {
+          direction = "north";
+          y += Integer.valueOf(lst.get(i).substring(1));
+        }
+      }
+      else if (direction.equals("west")) {
+        if (lst.get(i).contains("L")) {
+          direction = "south";
+          y -= Integer.valueOf(lst.get(i).substring(1));
+        } else {
+          direction = "north";
+          y += Integer.valueOf(lst.get(i).substring(1));
+        }
+      }
+    }
+    return Math.abs(x) + Math.abs(y);
   }
 }
