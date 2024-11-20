51f51b08bbb93d55afc8e52c6dbf9dc29dc60b1f
Jamie Li <jli60@stuy.edu>
2024-11-20 10:48:30 -0500

    finished Part 2 of Day 1

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -25,48 +25,58 @@ public class Day1 {
   }
 
   public static int EasterBunny(String filename) {
-    int x = 0;
+    ArrayList<int[]> ary = new ArrayList<int[]>();
+    int x = 0; 
     int y = 0;
-    String direction = "north";
+    int direction = 0;
+    int[] initial = new int[]{0,0};
     ArrayList<String> lst = readFile(filename);
+    int[][] coordinates = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
     for (int i = 0; i < lst.size(); i++) {
-      if (direction.equals("north")) {
-        if (lst.get(i).contains("R")) {
-          direction = "east";
-          x += Integer.valueOf(lst.get(i).substring(1));
-        } else {
-          direction = "west";
-          x -= Integer.valueOf(lst.get(i).substring(1));
-        }
+      if (lst.get(i).contains("R")) {
+        direction++;
+      } else {
+        direction--;
       }
-      else if (direction.equals("south")) {
-        if (lst.get(i).contains("L")) {
-          direction = "east";
-          x += Integer.valueOf(lst.get(i).substring(1));
-        } else {
-          direction = "west";
-          x -= Integer.valueOf(lst.get(i).substring(1));
+      direction = (direction + 4) % 4; 
+      x += coordinates[direction][0] * Integer.valueOf(lst.get(i).substring(1));
+      y += coordinates[direction][1] * Integer.valueOf(lst.get(i).substring(1));
+      ArrayList<int[]> temp = helper(x,y,initial);
+        if (ary.size() > 0) {
+          for (int j = 0; j < ary.size(); j++) {
+            for (int k = 0; k < temp.size(); k++) {
+              if (temp.get(k)[0] == ary.get(j)[0] && temp.get(k)[1] == ary.get(j)[1]) {
+                return Math.abs(ary.get(j)[0]) + Math.abs(ary.get(j)[1]);
+              }
+            }
+          }
         }
-      }
-      else if (direction.equals("east")) {
-        if (lst.get(i).contains("R")) {
-          direction = "south";
-          y -= Integer.valueOf(lst.get(i).substring(1));
-        } else {
-          direction = "north";
-          y += Integer.valueOf(lst.get(i).substring(1));
-        }
-      }
-      else if (direction.equals("west")) {
-        if (lst.get(i).contains("L")) {
-          direction = "south";
-          y -= Integer.valueOf(lst.get(i).substring(1));
-        } else {
-          direction = "north";
-          y += Integer.valueOf(lst.get(i).substring(1));
-        }
-      }
+      ary.addAll(temp);
+      initial = new int[]{x,y};
     }
     return Math.abs(x) + Math.abs(y);
+  }
+
+  public static ArrayList<int[]> helper(int x, int y, int[] initial) {
+    ArrayList<int[]> ary = new ArrayList<int[]>(); 
+    int xCount, yCount;
+    if (initial[0] < x) xCount = 1;
+    else xCount = -1; 
+
+    if (initial[1] < y) yCount = 1;
+    else yCount = -1;
+    
+    if (initial[0] == x) {
+    for (int i = initial[1]; i != y; i += yCount) {
+      ary.add(new int[]{x,i});
+      }
+    }
+
+    else if (initial[1] == y) {
+      for (int i = initial[0]; i != x; i+=xCount) {
+        ary.add(new int[]{i,y});
+        }
+      }
+    return ary;
   }
 }
