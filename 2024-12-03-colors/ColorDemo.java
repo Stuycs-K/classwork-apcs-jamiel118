295af705404b360affe779b9af2653b51b897301
Jamie Li <jli60@stuy.edu>
2024-12-04 03:12:26 -0500

    made some minor changes to cow and added rgbColors with parameter

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -13,6 +13,14 @@ public class ColorDemo{
     System.out.print( "\u001b[" + foreground + ";" + modifier + "m");
   }
 
+  public static String repeat(String str) {
+    String result = "";
+    for (int i = 0; i < str.length() + 3; i++) {
+      result += "_";
+    }
+    return result;
+  }
+
   public static void cow() {
     System.out.println(HIDE_CURSOR + CLEAR_SCREEN);
     Scanner input = new Scanner(System.in);
@@ -25,15 +33,37 @@ public class ColorDemo{
     System.out.println("Enter text:");
     String text = input.nextLine();
 
-    System.out.println("_________________________________________");
+    System.out.println(repeat(text) + "\n");
     System.out.println("<" + text + ">");
-    System.out.println("_________________________________________");
+    System.out.println(repeat(text));
     System.out.println("        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----- |\n                ||     ||");
     System.out.println(SHOW_CURSOR);
     System.out.print(Reset);
   }
 
+  public static void rgbColors(int increase) {
+    for(int r = 0; r < 256; r+=increase){
+      for(int g = 0; g <= 256; g+=increase){
+        for(int b = 0; b <= 256; b+=increase){
+          System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+        }
+      }
+        System.out.println();
+    }
+    System.out.print(Reset);
+  }
+
+  public static void sleep(int milli){
+    try{
+            Thread.sleep(milli);
+    }catch(Exception e){
+    }
+}
+
   public static void main(String[] args) {
+    //rgbColors(5);
+    //cow();
+    //sleep(3000);
     cow();
   }
 }
