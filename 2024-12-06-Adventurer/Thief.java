165471d5ae0c97d3b158824b238eb63fc2a14879
Jamie Li <jli60@stuy.edu>
2024-12-06 10:43:28 -0500

    created constructors and consumable accessor methods for Thief subclass

--- 2024-12-06-Adventurer/Thief.java
+++ 2024-12-06-Adventurer/Thief.java
@@ -1,4 +1,6 @@
 public class Thief extends Adventurer{
+  private String specialName = "wit";
+  private int specialMax, special = 30;
 
   //Constructors
   public Thief(String name) {
@@ -8,4 +10,20 @@ public class Thief extends Adventurer{
   public Thief(String name, int hp) {
     super(name,hp);
   }
+   //Consumable methods
+
+   public String getSpecialName() {
+     return specialName;
+   }
+   //accessor methods
+   public int getSpecial() {
+     return special;
+   }
+   public void setSpecial(int n); {
+     special = n;
+   }
+   public int getSpecialMax() {
+     return specialMax;
+   }
+
 }
