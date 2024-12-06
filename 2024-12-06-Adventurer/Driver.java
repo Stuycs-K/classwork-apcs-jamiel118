32690e808ea38a6742cdea8a86d8e8a125b77bdd
Jamie Li <jli60@stuy.edu>
2024-12-06 15:07:19 -0500

    created testers for accessor for consumables

---
+++ 2024-12-06-Adventurer/Driver.java
@@ -0,0 +1,17 @@
+public class Driver {
+    public static void main(String[] args) {
+        Adventurer a = new Thief("Bob",30);
+        System.out.println("Thief Bob");
+        System.out.println(a.getSpecialName());
+        System.out.println(a.getSpecial());
+        System.out.println(a.getSpecialMax());
+
+        Adventurer b = new Thief("Elon",30);
+        System.out.println("\nThief Elon");
+        System.out.println(a.getSpecialName());
+        System.out.println(a.getSpecial());
+        System.out.println(a.getSpecialMax());
+
+
+    }
+}
