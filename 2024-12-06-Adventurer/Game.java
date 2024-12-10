0f82e365be5b428a5566ea3639295d0e44420762
Jamie Li <jli60@stuy.edu>
2024-12-10 01:37:56 -0500

    added part of the loop for enemy action

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -9,11 +9,10 @@ public class Game {
     Scanner userInput = new Scanner(System.in);
 
     while(true) {
+      System.out.println(enemy.getName() + ", " + enemy.getHP() + " HP, " + enemy.getSpecial() + " " + enemy.getSpecialName());
+      System.out.println(player.getName() + ", " + player.getHP() + " HP, " + player.getSpecial() + " " + player.getSpecialName());
 
       if (player.getHP() > 0) {
-        System.out.println(enemy.getName() + ", " + enemy.getHP() + " HP, " + enemy.getSpecial() + " " + enemy.getSpecialName());
-        System.out.println(player.getName() + ", " + player.getHP() + " HP, " + player.getSpecial() + " " + player.getSpecialName());
-
         System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
         String input = userInput.nextLine();
 
@@ -21,22 +20,39 @@ public class Game {
           break;
         }
         else if (input.equals("sp")) {
-          player.specialAttack(enemy);
           System.out.println("You performed a special attack on the enemy");
+          System.out.println( player.specialAttack(enemy));
         }
         else if (input.equals("su")) {
-          player.support();
           System.out.println("You healed yourself");
+          System.out.println(player.support());
         }
         else if (input.equals("a")) {
-          player.attack(enemy);
           System.out.println("You attack the enemy");
+          System.out.println(player.attack(enemy));
         }
         else {
           System.out.println("Not a valid input, type (a)ttack / (sp)ecial / (su)pport / quit");
         }
       } else {
         System.out.println("You lost! The enemy won");
+        break;
+      }
+
+      if (enemy.getHP() > 0) {
+        int random = (int)(Math.random() * 3);
+        if (random == 0) {
+          System.out.println(enemy.attack(player));
+        }
+        else if (random == 1) {
+          System.out.println(enemy.specialAttack(player));
+        }
+        else if (random == 2) {
+          System.out.println(enemy.support());
+        }
+      }
+      else {
+        System.out.println("You won! The enemy is dead");
         break;
       }
     }
