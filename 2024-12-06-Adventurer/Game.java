6b4ffcc629fa93cfb15697923ebfa1f5d959d802
Jamie Li <jli60@stuy.edu>
2024-12-10 01:10:53 -0500

    created loop for player with user input

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Game {
 
   public static void main(String[] args) {
@@ -7,21 +9,36 @@ public class Game {
     Scanner userInput = new Scanner(System.in);
 
     while(true) {
-      System.out.println(enemy.getName() + ", " + enemy.getHP() + " HP, " + enemy.getSpecial() + " " + enemy.getSpecialName());
-      System.out.println(player.getName() + ", " + player.getHP() + " HP, " + player.getSpecial() + " " + player.getSpecialName());
 
-      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
-      String input = userInput.nextLine();
+      if (player.getHP() > 0) {
+        System.out.println(enemy.getName() + ", " + enemy.getHP() + " HP, " + enemy.getSpecial() + " " + enemy.getSpecialName());
+        System.out.println(player.getName() + ", " + player.getHP() + " HP, " + player.getSpecial() + " " + player.getSpecialName());
 
-      if(input.equals("a")) {
-        player.attack(enemy);
+        System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+        String input = userInput.nextLine();
+
+        if(input.equals("quit")) {
+          break;
+        }
+        else if (input.equals("sp")) {
+          player.specialAttack(enemy);
+          System.out.println("You performed a special attack on the enemy");
+        }
+        else if (input.equals("su")) {
+          player.support();
+          System.out.println("You healed yourself");
+        }
+        else if (input.equals("a")) {
+          player.attack(enemy);
+          System.out.println("You attack the enemy");
+        }
+        else {
+          System.out.println("Not a valid input, type (a)ttack / (sp)ecial / (su)pport / quit");
+        }
+      } else {
+        System.out.println("You lost! The enemy won");
+        break;
       }
-      if (input.equals("sp")) {
-        player.specialAttack(enemy);
-      }
-      if (input.equals("su")) {
-        player.support();
-      }
-      if (input.equals)
     }
+  }
 }
